8f6cc21ccf45a48c3f4a2712784d7934d36251fa
MegaSean3000 <sfp1216@gmail.com>
2025-02-11 21:58:41 -0500

    Update lab5-3.cpp

--- lab5-3.cpp
+++ lab5-3.cpp
@@ -2,11 +2,40 @@
 //Created/revised by <your name> on <current date>
 
 #include <iostream>
+#include <cmath>
 #include <iomanip>
 using namespace std;
+int main() {
+    double shipping_fee = 0.0;
+    double purchase_amount = 0.0;
+    double discount_rate = 0.0;
+    char club_member = ' ';
+    double total_price = 0.0;
+    double total_discount = 0.0;
+    double new_total = 0.0;
+    cout << "Enter purchase amount: " << endl;
+    cin >> purchase_amount;
+    cout << "Are you a Premier Club member? (Y/N)" << endl;
+    cin >> club_member;
 
-int main()
-{
+    if (toupper(club_member) == 'Y'){
+        discount_rate = 0.1;
+    }else
+        if (toupper(club_member) == 'N'){
+        discount_rate = 0.05;
 
-	return 0;
-}	//end of main function
+    }else {
+    cout << "Invalid number entered. Please restart";
+    }
+    total_discount = (purchase_amount * discount_rate);
+    total_price = (purchase_amount - total_discount);
+        if (total_price >= 100) {
+        shipping_fee = 0.99;
+    } else {
+        shipping_fee = 4.99;
+    }
+    new_total = total_price + shipping_fee;
+
+
+    cout << "Total price: $" << new_total << endl;
+}
