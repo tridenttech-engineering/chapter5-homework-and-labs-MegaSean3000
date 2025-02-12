8419095058be0c35150d8f88edfa1469e749107c
MegaSean3000 <sfp1216@gmail.com>
2025-02-12 15:51:17 -0500

    Update lab5-3.cpp

--- lab5-3.cpp
+++ lab5-3.cpp
@@ -1,41 +1,38 @@
-//lab5-3.cpp - displays the total amount due
-//Created/revised by <your name> on <current date>
+  //Lab5-2.cpp - displays the total amount due
+  //Created/revised by <your name> on <current date>
 
-#include <iostream>
-#include <cmath>
-#include <iomanip>
-using namespace std;
-int main() {
-    double shipping_fee = 0.0;
-    double purchase_amount = 0.0;
-    double discount_rate = 0.0;
-    char club_member = ' ';
-    double total_price = 0.0;
-    double total_discount = 0.0;
-    double new_total = 0.0;
-    cout << "Enter purchase amount: " << endl;
-    cin >> purchase_amount;
-    cout << "Are you a Premier Club member? (Y/N)" << endl;
-    cin >> club_member;
+  #include <iostream>
+  #include <iomanip>
+  using namespace std;
 
-    if (club_member) == 'Y'){
-        discount_rate = 0.1;
-    }else
-        if (club_member) == 'N'){
-        discount_rate = 0.05;
-
-    }else {
-    cout << "Invalid number entered. Please restart";
-    }
-    total_discount = (purchase_amount * discount_rate);
-    total_price = (purchase_amount - total_discount);
-        if (total_price >= 100) {
-        shipping_fee = 0.99;
-    } else {
-        shipping_fee = 4.99;
-    }
-    new_total = total_price + shipping_fee;
+  int main()
+  {
+     const double DISCOUNT_RATE_1 = 0.1;
+     const double DISCOUNT_RATE_2 = 0.05;
+     const double SHIP_CHG1 = 0.99;
+     const double SHIP_CHG2 = 4.99;
+     double amtOwed = 0.0;
+     char member = ' ';
 
 
-    cout << "Total price: $" << new_total << endl;
-}
+     cout << "Amount owed before any discount and shipping: ";
+     cin >> amtOwed;
+     cout << "Premier Club member (Y/N)? ";
+     cin >> member;
+
+     if (toupper(member) == 'Y')
+         amtOwed -= amtOwed * DISCOUNT_RATE_1;
+     else
+         amtOwed -= amtOwed * DISCOUNT_RATE_2;
+
+     if (amtOwed >= 100.0)
+         amtOwed += SHIP_CHG1;
+     else
+         amtOwed += SHIP_CHG2;
+
+     cout << fixed << setprecision(2);
+     cout << "Amount owed after any discount and shipping: "
+         << amtOwed << endl;
+
+     return 0;
+ }
