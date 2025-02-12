deb1b9120d503c9abe807072fc154acaa8087b74
MegaSean3000 <sfp1216@gmail.com>
2025-02-12 14:49:36 -0500

    Update hw5-12.cpp

--- hw5-12.cpp
+++ hw5-12.cpp
@@ -1,12 +1,36 @@
 //hw5-12.cpp - displays the total owed
 //Created/revised by <your name> on <current date>
 
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main()
 {
+    double price_1 = 0;
+    double price_2 = 0;
+    double total_price = 0;
+    cout << "Enter price for first product" << endl;
+    cin >> price_1;
+    cout << "Enter price for second product" << endl;
+    cin >> price_2;
 
-	return 0;
-}	//end of main function
+
+    if (price_1 > price_2) {
+
+        price_2 = price_2 / 2;
+
+    } else if (price_2 > price_1) {
+
+        price_1 = price_1 / 2;
+
+    } else if (price_2 == price_1) {
+
+        price_2 = price_2 / 2;
+    }
+
+    total_price = price_1 + price_2;
+
+    cout << "Total price is $" << total_price << endl;
+}   //end of main function
